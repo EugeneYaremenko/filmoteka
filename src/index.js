dcb23a356bf1f465560087d91320df07943e1f62
EugeneYaremenko <yaremenko.e.s@gmail.com>
2020-08-26 18:25:28 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import './js/pixabay-search';
+// import './js/services/pixabayAsync-services';
+// import './js/spinner';
+// import './js/buttonScrollTop';
+// import './js/services/notices';
+// import './js/gallery-item';
+
+import './sass/main.scss';
+
