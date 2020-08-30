c02c8cfa4382648291069c8796e9aa6d3c03f675
unknown <azimov3.9.1991@gmail.com>
2020-08-30 12:52:11 +0300

    searchAndPlaginationHomePage3

--- src/js/searchAndPlaginationHomePage.js
+++ src/js/searchAndPlaginationHomePage.js
@@ -3,21 +3,27 @@ import cardTemplate from '../templates/cardFilm.hbs';
 let inputValue;
 let pageNumber = 1;
 let renderFilms;
+const baseUrl = 'https://api.themoviedb.org/3/search/movie';
 
 const refs = {
   searchForm: document.querySelector('#js-form'),
   filmList: document.querySelector('#js-film-list'),
   errorMessage: document.querySelector('#js-error'),
+  paginationBlock: document.querySelector('#js-pagination-wrapper'),
+  prevButton: document.querySelector('#js-prev-button'),
+  nextButton: document.querySelector('#js-next-button'),
+  pageNum: document.querySelector('#js-page-number'),
 };
 
 refs.searchForm.addEventListener('submit', searchFilms);
+refs.paginationBlock.addEventListener('click', plaginationNavigation);
+// refs.prevButton.addEventListener('click', plaginationNavigation);
+// refs.nextButton.addEventListener('click', plaginationNavigation);
 
 function searchFilms(e) {
   e.preventDefault();
 
   inputValue = e.currentTarget.elements.query.value;
-
-  console.log(inputValue);
 
   fetchFilms(inputValue);
   // fetchPopularMoviesList();
@@ -27,34 +33,45 @@ function clearFilmList() {
   refs.filmList.innerHTML = '';
 }
 
-const baseUrl = 'https://api.themoviedb.org/3/search/movie';
-
-//   'https://api.themoviedb.org/3/search/movie?api_key=0e322ad2a3bf93179a3983749fdc0c73&query=terminator'
-
 function fetchFilms(inputValue) {
   const key = '0e322ad2a3bf93179a3983749fdc0c73';
-
   const requestParams = `?api_key=${key}&query=${inputValue}&page=${pageNumber}`;
   fetch(baseUrl + requestParams)
     .then(response => response.json())
     .then(data => {
       renderFilms = data;
-
       console.log(renderFilms);
       if (renderFilms.total_results == 0) {
-        document.querySelector('#js-error').style = 'display: inherit';
+        document.querySelector('#js-error').classList.remove('visually-hidden');
       } else {
         clearFilmList();
-         insertListItems(data.results)
+        insertListItems(data.results);
+
         //   createCardFunc(data.results);
       }
+      if (pageNumber <= 1) {
+        refs.prevButton.classList.add('visually-hidden');
+      } else {
+        refs.prevButton.classList.remove('visually-hidden');
+      }
     });
-
 }
-
-
 
 async function insertListItems(images) {
   const markupGallery = await cardTemplate(images);
   refs.filmList.insertAdjacentHTML('beforeend', markupGallery);
+}
+
+function plaginationNavigation(e) {
+  event.preventDefault();
+
+  if (event.target.nodeName == 'BUTTON') {
+    if (event.target.name == 'Prev') {
+      pageNumber -= 1;
+    } else if (event.target.name == 'Next') {
+      pageNumber += 1;
+    }
+
+    fetchFilms(inputValue);
+  }
 }
