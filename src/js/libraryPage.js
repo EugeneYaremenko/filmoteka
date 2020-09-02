4e5e7cc1b1611074a1d00d03af922c066fc3afa5
ivanberezin <ivanberezin@ukr.net>
2020-09-02 23:58:24 +0300

    2020-09-03 - library ok

--- src/js/libraryPage.js
+++ src/js/libraryPage.js
@@ -1,32 +1,26 @@
 import cardLibraryFilm from '../templates/cardLibraryFilm.hbs';
+import { activeDetailsPage } from './navigation';
 
 let buttonLibraryQueue = document.querySelector(
   '#js-navigation-library-btn-queue',
 );
-
 let buttonLibraryWatched = document.querySelector(
   '#js-navigation-library-btn-watched',
 );
-
 let libraryFilmList = document.querySelector('#js-library-film-list');
 
 function createLibraryCardFunc(imgPath, filmTitle, movieId, voteAverage) {
   const createLi = document.createElement('li');
   createLi.classList.add('main__film-item');
-
   const createLiMarkup = `<div class="main__film-item-wrapper">
       <a class="main__film-link js-film-link">
         <img alt="${filmTitle}" src="https://image.tmdb.org/t/p/w500/${imgPath}" class="main__film-image js-film-image">
       </a>
     </div>
-
     <span class="main__library-film-list-title">${filmTitle}</span>
     <span class="main__library-film-list-popularity">Popularity: ${voteAverage}</span>`;
-
   createLi.insertAdjacentHTML('beforeend', createLiMarkup);
-
   createLi.addEventListener('click', () => activeDetailsPage(movieId, true));
-
   return createLi;
 }
 
@@ -53,10 +47,8 @@ function drawQueueFilmList() {
 
   if (filmsQueueLocalStorage === null || parseLocalStorage.length === 0) {
     libraryFilmList.innerHTML = '';
-
     const emptyLibrary =
       '<li class="empty">You do not have to queue movies to watch. Add them.</li>';
-
     libraryFilmList.insertAdjacentHTML('afterbegin', emptyLibrary);
   }
 
@@ -86,12 +78,11 @@ function drawWatchedFilmList() {
 
   if (filmsWatchedLocalStorage === null || parseLocalStorage.length === 0) {
     libraryFilmList.innerHTML = '';
-
     const emptyLibrary =
       '<li class="empty">You do not have watched movies. Add them.</li>';
-
     libraryFilmList.insertAdjacentHTML('afterbegin', emptyLibrary);
   }
+
   buttonLibraryQueue.classList.remove('main__navigation-library-btn-active');
   buttonLibraryWatched.classList.add('main__navigation-library-btn-active');
 }
